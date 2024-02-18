934d0e7821b901977ce79d6e45c7b7b9c22fced8
Abdurahim102 <abundance0121@gmail.com>
2024-02-18 16:15:37 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,108 @@
+
+
+// let num = prompt('Nechi yosh')
+
+// switch (num) {
+//     case '18':
+//        console.log(alert('Togri keladi')); 
+//         break;
+//     case '17':
+//         console.log(alert('Mumkun emas'));
+//         break
+//     default:
+            // console.log(alert('18 yoki 17 yozing'));
+//         break;
+// }
+
+
+// let num2 = prompt('Nechi narsa kerak')
+
+// switch (num2) {
+//     case '5':
+//        console.log(alert('Boladi'));     
+//         break;
+//     case '6':
+//         console.log(alert('Hozircha yok'));
+//         break
+//     default:
+//         console.log(alert('kop tovar yok'));
+//         break;
+// }
+
+// let num3 = 3;
+// let sor = +prompt('Menda raqam bor top birdan uchgacha')
+// switch (sor) {
+//     case num3:
+//        console.log(alert('topdin'));    
+//         break;
+//     default:
+//         console.log(alert('topalmadin'));
+//         break;
+// }
+
+
+
+// let num4 = prompt('Qaysi kun')
+// switch (num4) {
+//     case 'dushanba':
+//        console.log(alert('dushanba bugun hop'));    
+//         break;
+//     default:
+//         console.log(alert('Bugun dushanba emas'));
+//         break;
+// }
+
+
+
+// let login = prompt('Login')
+// let password = prompt('Parol')
+
+// function info(pass, log) {
+//     if (login == log && password == pass) {
+//         console.log(alert('Ottingiz'));
+//     }else{
+//     console.log(alert('Otmadiz'));
+//     }
+// }
+// info('qwerty123','Abdurahim');
+
+
+// let num4 = +prompt('raqam')
+// let num5 = +prompt('raqam2')
+
+// function get(num1,num2) {
+//     console.log(alert(`${num1} + ${num2} = ${num1 + num2}`));
+// }
+
+// get(num4,num5)
+
+// let pro = prompt('qaysi')
+
+// function rol(kon1,kon2) {
+//     if (kon1 == pro ) {
+//     console.log('Bugun dushanba');
+        
+//     }else if (kon2 == pro){
+//     console.log('Bugun chorchanba');
+//     }
+// }
+// rol('dushanba','chorchanba');
+
+
+// function nu(sym,sym2,sym3) {
+//     console.log(sym,sym2,sym3);
+// }
+// nu('-','+','=')
+
+
+let abd = prompt('Ism')
+
+function pro(name) {
+    if (abd == name) {
+        console.log('Togri keldi');
+    }else{
+    console.log('togri emas');
+    }
+}
+
+pro('Abdurahim');
